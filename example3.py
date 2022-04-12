98501117b0fe085327c2d1e32ca528f3574ece42
lehitom <48111403+lehitom@users.noreply.github.com>
2022-04-12 15:37:45 -0600

    testing different pixel layouts

--- example3.py
+++ example3.py
@@ -1,19 +1,24 @@
+from tkinter import Frame, Tk
 
 
-from tkinter import Label, Widget
+center = Tk()
+center.geometry('455x455')
+center.title("9x9 grid")
+
+cells = {}
+for row in range(9):
+     for column in range(9):
+        cell = Frame(center, bg='white', highlightbackground="black",
+                     highlightcolor="black", highlightthickness=1,
+                     width=50, height=50, padx=3, pady=3)
+        cell.grid(row=row, column=column)
+        cells[(row, column)] = cell
 
 
-widgets_dictionary = {} # initialize your empty dictionary
+def color_cell(cells, i, j, color="red"):
+    cells[(i, j)].configure(background="red")
 
-for i in range(rows):
-    for j in range(columns):
-        # widget definition here, let's say it's a label
-        label = Label(...)
-        label.pack()
-        # create a reference to the widget in your dictionary
-        widget[(i,j)] = label
 
-# Now, referring to your specific question:
-# (x,y) = the index of the cell you want to change
-# colour = the background colour you want to change it to
-Widget[(x,y)].config(bg = "colour") 
+center.after(5000, color_cell, cells, 3, 4)
+
+center.mainloop()
